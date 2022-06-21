740ce7ecd31bf44582e577c14c5a5d8a2fe9f7d9
Connor Semb <polishmustard710@gmail.com>
2022-06-21 11:51:14 -0500

    initial transfer of files to new directory/repository

---
+++ seeds/index.js
@@ -0,0 +1,19 @@
+const seedPosts = require('./postSeeds');
+const seedUsers = require('./userSeeds');
+const seedComments = require('./commentSeeds');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  
+  await seedUsers();
+  
+  await seedPosts();
+
+  await seedComments();
+
+  process.exit(0);
+};
+
+seedAll();
